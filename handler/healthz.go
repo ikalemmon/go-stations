3eda5acb205895007d90c670b6504063db4d2e2c
ikalemmon <nagatadaiki@MacBook-Pro-142.local>
2022-02-22 17:16:21 +0900

    5-3

--- handler/healthz.go
+++ handler/healthz.go
@@ -20,7 +20,6 @@ func NewHealthzHandler() *HealthzHandler {
 // HealthHandlerに関する処理を書く。
 func (h *HealthzHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	var response = &model.HealthzResponse{}
-	fmt.Print(response)
 	//fmt.Fprint(w, "Hello World from Go.")
 	w.Header().Set("Content-Type", "application/json")
     json.NewEncoder(w).Encode(response)
