c9aa99b7eebf112a8f8297f34101f60f28940d57
ikalemmon <nagatadaiki@MacBook-Pro-142.local>
2022-12-06 21:24:13 +0900

    accessLog

--- handler/middleware/OS.go
+++ handler/middleware/OS.go
@@ -2,7 +2,10 @@ package middleware
 
 import (
 	"context"
+	"encoding/json"
+	"fmt"
 	"net/http"
+	"time"
 
 	"github.com/mileusna/useragent"
 )
@@ -19,6 +22,30 @@ func OSname(h http.Handler) http.Handler {
 		r = r.Clone(ctx)
 
 		h.ServeHTTP(w, r)
+	}
+	return http.HandlerFunc(fn)
+}
+
+type Log struct {
+	TimeStamp time.Time `json:"time_stamp"`
+	Latency   int64     `json:"latency"`
+	Path      string    `json:"path"`
+	OS        string    `json:"os"`
+}
+
+func acccessLog(h http.Handler) http.Handler {
+	fn := func(w http.ResponseWriter, r *http.Request) {
+		start := time.Now()
+		h.ServeHTTP(w, r)
+		end := time.Now()
+		var latency = (end.UnixNano() / int64(time.Millisecond)) - (start.UnixNano() / int64(time.Millisecond))
+		var log = Log{}
+		log.TimeStamp = time.Now()
+		log.Latency = latency
+		log.Path = r.URL.Path
+		h = OSname(h)
+		log.OS = r.Context().Value("OS").(string)
+		fmt.Println(json.NewEncoder(w).Encode(log))
 	}
 	return http.HandlerFunc(fn)
 }
---
+++ handler/middleware/accessLog.go
@@ -0,0 +1,4 @@
+package middleware
+
+//ここにアクセスログを出力するmiddlewareを描こうとしたが、cycle importのエラー。
+//OSを取得する際にmiddlewareのOSを利用するので生じるエラー。解消するには、別フォルダに写すしかなさそう。
